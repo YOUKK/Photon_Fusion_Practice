adeeae25e092b9bac1d12a149015dbe530171070
YOUKK <kimyoukeng@naver.com>
2025-06-10 17:10:21 +0900

    host/join 기능과 플레이어 이동

---
+++ Assets/Scripts/NetworkInputData.cs
@@ -0,0 +1,9 @@
+using Fusion;
+using UnityEngine;
+
+// 사용자로부터 입력을 수집 -> 호스트(서버 역할)에게 입력 데이터 전달 -> 모든 클라이언트에게 동기화
+// OnInput에서 사용한 Set()이 T를 managed로 제한하고 있어 struct 사용
+public struct NetworkInputData : INetworkInput
+{
+    public Vector3 direction;
+}
